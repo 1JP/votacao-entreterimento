29072311163e02419e4e85b61f104d65bb52c1d5
1JP <joaopedrogs1@outlook.com>
2024-09-01 11:43:38 -0300

    models and migrations

---
+++ database/migrations/2024_09_01_142027_create_plans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->integer('number_film');
+            $table->integer('number_book');
+            $table->integer('number_serie');
+            $table->decimal('value', 8 , 2);
+            $table->string('customer_id', 100);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
